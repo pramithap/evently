8aa1f3f0bfa2a687d1007f73b2e92e064ab02b63
pramithap <pramithap@gmail.com>
2024-01-23 09:59:54 +0530

    removed unwanted code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import { Header } from "@/components/ui/shared/Header";
-import Footer from "@/components/ui/shared/Footer";
 
 import { ClerkProvider } from "@clerk/nextjs";
 
