c12e920f0e880d54ac84c97225d1876f3a762c59
pramithap <pramithap@gmail.com>
2024-03-01 08:27:46 +0530

    Added webhook secret

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -11,8 +11,8 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  //const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
-  const WEBHOOK_SECRET = undefined;
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  //const WEBHOOK_SECRET = undefined;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
